33f207dfc662f30e9c3b612023cd0bb347a8ba4d
DiegoMC <damc99@hotmail.com>
2024-10-26 19:36:15 -0500

    agregando_gettypesservice

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\DiscountController;
 use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\TypeOfServiceController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VariableController;
 use Illuminate\Http\Request;
@@ -13,3 +14,4 @@ Route::resource('/appointment', AppointmentController::class);
 Route::resource('/variable', VariableController::class);
 Route::resource('/service', ServiceController::class);
 Route::resource('/discount', DiscountController::class);
+Route::resource('/type', TypeOfServiceController::class);
